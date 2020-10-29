43d8a5ff24cd52253b2ba65ca49715f21790069c
Kagayakashi <kagayakashi.vo@gmail.com>
2020-10-29 18:16:39 +0000

    Добавил связь Тестов и Юзеров к списку проходящих тестов при удалении.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :сreator, :class_name => 'User'
   belongs_to :category
   
-  has_many :users, through: :tests_users
   has_many :tests_users, dependent: :destroy
+  has_many :users, through: :tests_users
   has_many :questions, dependent: :destroy
 
   def self.tests_name_with_category_desc(category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   belongs_to :test
   
-  has_many :tests_users
+  has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
   
   def tests_with_level(level)
