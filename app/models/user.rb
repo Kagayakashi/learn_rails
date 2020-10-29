5a31f21d45b7ebd4c320129b2d4e61380f9ce66f
Kagayakashi <kagayakashi.vo@gmail.com>
2020-10-29 18:19:44 +0000

    Добавил алиас к списку тестов выбранного пользователя. Изменил ассоциацию.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   belongs_to :test
   
   has_many :tests_users, dependent: :destroy
-  has_many :tests, through: :tests_users
+  has_many :testings, through: :tests_users, source: :test
   
   def tests_with_level(level)
     tests.find_by(level: level)
