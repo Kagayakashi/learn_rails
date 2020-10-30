9b6abfdb4ffa9295f9e8f5b4ed4aa5b26ab5308b
Kagayakashi <kagayakashi.vo@gmail.com>
2020-10-30 14:08:56 +0000

    Изменена выборка тестов у пользователя. Изменена версия.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :testings, through: :tests_users, source: :test
   
   def tests_with_level(level)
-    tests.find_by(level: level)
+    tests.where(level: level)
   end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 module LearnRails
   class Application < Rails::Application
-    VERSION = '0.5.2'.freeze
+    VERSION = '0.5.3'.freeze
   end
 end
