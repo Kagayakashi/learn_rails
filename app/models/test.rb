69d7d5a5abb26f013c21c279ecd658b44e0a8a90
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-03 20:51:05 +0000

    Значение уровня теста может быть больше или равно 0.

--- app/models/test.rb
+++ app/models/test.rb
@@ -20,5 +20,5 @@ class Test < ApplicationRecord
   
   validates :title, presence: true,
                     uniqueness: true
-  validates :level, numericality: { only_integer: true }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
