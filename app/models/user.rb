33942e54887a33a2021e6f1ba5c9748dbb0fd7be
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-03 21:47:31 +0000

    Валидация данных Пользователя. Задча 6.7.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :testings, through: :tests_users, source: :test
   
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+  
   def tests_with_level(level)
     tests.where(level: level)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_211749) do
+ActiveRecord::Schema.define(version: 2020_11_03_214224) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_211749) do
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "answers", "questions"
