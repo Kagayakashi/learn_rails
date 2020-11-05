d340c5146fae94cf0d1e0297f83c7fcd2bdb4208
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-05 13:40:23 +0000

    Обновлена версия.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :testings, through: :tests_users, source: :test
   
-  validates :username, :password, :email presence: true
+  validates :username, :password, :email, presence: true
   
   def tests_with_level(level)
     tests.where(level: level)
