f23295f3dd9fbc2eba0d938de5130c48c7900271
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-05 13:08:51 +0000

    Переместил default scope в верх модуля.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
-  has_many :tests, dependent: :nullify
-  
   default_scope { order(title: :asc) }
+
+  has_many :tests, dependent: :nullify
   
   validates :title, presence: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
+  default_scope { order(created_at: :desc) }
+
   belongs_to :creator, :class_name => 'User'
   belongs_to :category, optional: true
   
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
   has_many :questions, dependent: :destroy
-
-  default_scope { order(created_at: :desc) }
   
   scope :easy, -> { where(level: 0..1).order(created_at: :desc) }
   scope :normal, -> { where(level: 2..4).order(created_at: :desc) }
