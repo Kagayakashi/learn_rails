edcf83d6833aa728e8948fab9341df35ecc51a24
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-11 21:39:19 +0000

    Сделал примеры роутинга. Добавил ресурс тест со вложенным ресурсом вопросы.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Resources examples
+  ## Default actions
+  # - resources :cars
+  # - resources :dealer, controller: :seller
+  # - resources :cars, only: %i[index show]
+  # - resources :cars do
+  #     resources :models
+  #   end
+  # - resources :cars do
+  #     resources :models, shallow: true
+  #   end
+  ## Custom actions
+  # - resources :models do
+  #     get :discount, on: :collection
+  #     member do # member = each item from resource
+  #       post :test_drive
+  #     end
+  #   end
+  
+  # Non-resources examples
+  # - get '/cars', to 'cars#index'
+  # - get '/cars/:id', to: 'cars#show', as: :test
+  # - get '/cars(/:id)', to: 'cars#show'  # ID optional
+  # - get '/models/:id/test_drive', to: 'models#test_drive'
+  #
+  # - match '/cars/:id', to 'cars#todo', via: %i[post get put]
+  # transfer http methods post get put from /cars/1 to controller method cars->todo
+  # - match '/cars/:id', to 'cars#todo', via: :all
+  
+  # root to: models#newest # home page is newest car model
+  
+  # exec - rails routes
+  
+  resources :tests do
+    resources :questions, shallow: true
+  end
+  
+  get '/tests/:id/start', to: 'tests#start'
+  
+  root to: 'tests#index'
+  
 end
