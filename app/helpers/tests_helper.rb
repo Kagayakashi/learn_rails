799f955d9834620fa5ec096d2d452ecaf7811234
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-18 16:01:35 +0000

    Хелпер для отображения в форме заголовка теста: 'редактирование' или 'создание'

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -11,4 +11,9 @@ module TestsHelper
   def test_level(test)
     TEST_LEVELS[test.level] || :Сложный
   end
+  
+  def test_header(test)
+    text = test.persisted? ? "Редактирвоание теста #{test.title}" : "Создание нового теста"
+    render inline: "<h1>#{text}</h1>"
+  end
 end
