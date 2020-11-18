7a85f261ff884c14c23149a3b05cded1e2f3784a
Kagayakashi <kagayakashi.vo@gmail.com>
2020-11-18 15:43:21 +0000

    Сделал helper для ссылки на github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,9 @@ module ApplicationHelper
     return Time.current.year if first_year == Time.current.year
     "2020-#{Time.current.year}"
   end
+  
+  def link_to_github(author, repo, link_name)
+    link_to link_name, "https://github.com/#{author}/#{repo}", target: :_blank
+  end
 
 end
