919e59e131919a7514230f0477542cca8641b489
kagayakashi <kagayakashi.vo@gmail.com>
2021-11-09 22:55:10 +0200

    Обновил конфиг почты

--- home/vo/learn_rails/config/environments/production.rb
+++ home/vo/learn_rails/config/environments/production.rb
@@ -64,13 +64,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'vo-learn-rails.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true }
+    port: 587,
+    address: 'smtp.gmail.com',
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
