1e9e55d09fa31e933dc00cb90e05ba09c094a495
kagayakashi <kagayakashi.vo@gmail.com>
2021-11-09 22:45:38 +0200

    Изменил домен в настройках почты и переменную окружения username

--- home/vo/learn_rails/config/environments/production.rb
+++ home/vo/learn_rails/config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'herokuapp.com',
+    domain:               'gmail.com',
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
