4d78f0ecfd4f629f790728387be6e3b31747b961
kagayakashi <kagayakashi.vo@gmail.com>
2021-11-19 12:45:16 +0200

    Сделал выдачу награды за определенные категории и уровни

--- app/services/reward_service/reward_issue.rb
+++ app/services/reward_service/reward_issue.rb
@@ -6,24 +6,51 @@ module RewardService
 
     class << self
       def issue(test_passage)
-        @@test_passage = test_passage
-
-        RewardIssue.first_try?
+        RewardIssue.first_try?(test_passage)
+        RewardIssue.by_level?(test_passage)
+        RewardIssue.by_category?(test_passage)
       end
 
-      def first_try?
-        return if TestPassage.where(user: @@test_passage.user, test: @@test_passage.test).count != 1
+      def first_try?(test_passage)
+        return if TestPassage.where(user: test_passage.user, test: test_passage.test).count != 1
+
         reward = Reward.where(rule_type: RULE_TYPE_FIRST_TRY.to_s).first
         return if reward.nil?
-        @@test_passage.user.issued_rewards.build(reward: reward).save
+        test_passage.user.issued_rewards.build(reward: reward).save
       end
 
-      def by_level?(reward)
-        # TODO
+      def by_level?(test_passage)
+        test = test_passage.test
+        completed_tests_by_level = Test
+                                     .where(level: test.level)
+                                     .joins(:test_passages)
+                                     .where(test_passages: { user: test_passage.user })
+                                     .uniq
+
+        tests_by_level = Test.where(level: test.level)
+
+        return if completed_tests_by_level.count != tests_by_level.count
+        reward = Reward.where(rule_type: RULE_TYPE_BY_LEVEL.to_s).first
+        return if reward.nil?
+        test_passage.user.issued_rewards.build(reward: reward).save
       end
 
-      def by_category?(reward)
-        # TODO
+      def by_category?(test_passage)
+        category = test_passage.test.category
+        completed_tests_by_category = Test
+                                        .joins(:category)
+                                        .where(categories: { title: category.title })
+                                        .joins(:test_passages)
+                                        .where(test_passages: { user: test_passage.user })
+                                        .uniq
+        tests_by_category = Test
+                              .joins(:category)
+                              .where(categories: { title: category.title })
+
+        return if completed_tests_by_category.count != tests_by_category.count
+        reward = Reward.where(rule_type: RULE_TYPE_BY_CATEGORY.to_s).first
+        return if reward.nil?
+        test_passage.user.issued_rewards.build(reward: reward).save
       end
     end
   end
